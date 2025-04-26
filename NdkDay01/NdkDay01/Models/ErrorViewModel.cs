a1bf5c80475f91543f360e7795b0d37d3ee9e8a1
Khánh Nguyễn <duykhanhduong088@gmail.com>
2025-04-27 01:53:16 +0700

    Bt1

---
+++ NdkDay01/NdkDay01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NdkDay01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
